7fbdb0fb76a9964342f7a6268ca3b8194018c504
Karina Yuja <66886321+KariLinux@users.noreply.github.com>
2024-02-28 23:42:34 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// const name = 'yoshi'
+
+// console.log(name); 
+
+// function 
+const greet = (name) => 
+{
+    console.log(`hello, ${name}`);
+}
+
+// call the fxn and pass it mario 
+greet('mario')
+greet('Yoshi')
